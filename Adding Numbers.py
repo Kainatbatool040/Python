0585b817a80106f63a14a9d3ba17e44a43b33ce6
Kainat_Batool_040 <91284782+Kainatbatool040@users.noreply.github.com>
2021-10-17 12:40:08 +0500

    Add files via upload

---
+++ Adding Numbers.py
@@ -0,0 +1,9 @@
+# Store input numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print(sum)5
