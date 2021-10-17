dcdb0abf2d4d7a5caf4a262a613c1acfebc8d9a4
Kainat_Batool_040 <91284782+Kainatbatool040@users.noreply.github.com>
2021-10-17 12:41:01 +0500

    Add files via upload

---
+++ Bonus Amount.py
@@ -0,0 +1,10 @@
+
+var1 = int(input("Enter your salary: "))
+var2 = int(input("Enter your Service year: "))
+if var2>5:
+    var3=var1/100 * 5
+    print("Bonus")
+    print(var3)
+else:
+    print("Illegible")
+    
